5f895c2f7215079244927082198af27ba3e392c0
wooseungw <97976494+wooseungw@users.noreply.github.com>
2023-05-01 18:32:01 +0900

    123

--- ESM/home/models.py
+++ ESM/home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+# class user (models.Model):
+#     id = models.IntegerField(Pri not NULL)
+#     api = models.CharField(max_length=200 not NULL)
+#     name = models.CharField(max_length=10)
+    
+    
+# class inputfile (models.Model):
+    
+    
+#     name = models.CharField(max_length=100)
+#     type = models.CharField(max_length=5)
+#     contents = models.TextField(default=None) 
+    
+    
+    
+# class summary (models.Model):
+#     user_api = models.ForeignObject(user,on_delete=models.CASCADE,from_fields=['api'],to_)
+#     in_contents = models.OneToOneField(inputfile)
+#     su_contents = models.TextField(default=None)
+#     su_impword =  models.TextField(default=None)
+    
+# class extent(models.Model):
+    
