003fa3a60cd1766bdda372e5945df7abe161f62b
Ccolornyong <ccolornyong@gmail.com>
2023-05-06 00:59:13 +0900

    12

--- ESM/home/views.py
+++ ESM/home/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render, redirect
 from .models import TUser
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect# 홈으로 이동하기 위해 redirect 추가로 적어준다
+
 
 # Create your views here.
 def home(request):
-    
-        return render(request, 'home.html')
-
+     if request.method == 'POST':
+        return redirect('/summary')
+     else:
+         return render(request, 'home.html')
 
 
 def summary(request):
-    return render(request, 'summary.html')
+    
+       return render(request, 'summary.html')
 
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['id']
-        password = request.POST['pwd']
-        # user = authenticate(request, username=TUser.us_id, password=TUser.us_pw)
-
-        # if user is not None:
-        #     login(request, user)
-        return redirect('/')  # 루트 위치로 리다이렉트합니다.
-        
-    else:
+     if request.method == 'POST':
+        user = TUser()
+        user.us_id = request.POST['id']
+        user.us_pw = request.POST['pwd']
+        user.save()
+        return redirect('/')
+     else:
         return render(request, 'login.html')
+
 
 def community(request):
     return render(request, 'community.html')
